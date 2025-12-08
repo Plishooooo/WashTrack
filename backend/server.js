064101732f630d644a1521d53f7c4792fe89cd81
Plishooooo <cristobal.simplicio.juico@gmail.com>
2025-12-09 02:48:00 +0800

    fixing database connection

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '061504',
+  password: 'SClxzIREJAGhmcjuxdtcJvWVKJETFDzJ',
   database: 'washtrack_db',
 });
 
